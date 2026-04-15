8e7ebec8c864a46926a0679782704c939c544df8
tekuuu <tekalign3330@gmail.com>
2026-04-16 01:47:57 +0300

    v0.6 entrypoint

--- packages/bundler/src/BundlerConfig.ts
+++ packages/bundler/src/BundlerConfig.ts
@@ -125,7 +125,7 @@ export const DebugBundlerConfigShape = {
 export const bundlerConfigDefault: Partial<BundlerConfig> = {
   port: '3000',
   privateApiPort: '3001',
-  entryPoint: '0x0000000071727De22E5E9d8BAf0edAc6f37da032',
+  entryPoint: '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789',
   senderCreator: '0x449ED7C3e6Fee6a97311d4b55475DF59C44AdD33',
   unsafe: false,
   conditionalRpc: false,
